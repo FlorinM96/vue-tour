e50117451e22774fc18dcc8bcca76e50881e9e09
FlorinM96 <44477450+FlorinM96@users.noreply.github.com>
2021-02-18 11:03:55 +0100

    ENH: add slot name

--- src/components/VStep.vue
+++ src/components/VStep.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="v-step" :id="'v-step-' + hash" :ref="'v-step-' + hash">
-    <slot>
+    <slot name="container">
       <slot name="header">
         <div v-if="step.header" class="v-step__header">
           <div v-if="step.header.title" v-html="step.header.title"></div>
